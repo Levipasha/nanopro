61823676bc1928700368c2537c9773b494d31221
Levipasha <abbupasha61@gmail.com>
2026-05-02 14:24:38 +0530

    Add 'Art' button to mobile bottom navigation

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6660,6 +6660,20 @@ function Profile() {
 
             <button
               type="button"
+              className={`dash-mobile-bottom-btn ${activeTab === 'link-art' ? 'dash-mobile-bottom-btn-active' : ''}`}
+              onClick={() => setActiveTab('link-art')}
+            >
+              <div className="dash-mobile-bottom-btn-icon">
+                <svg viewBox="0 0 24 24" width="18" height="18" fill="none" stroke="currentColor" strokeWidth="2">
+                  <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
+                  <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
+                </svg>
+              </div>
+              <span>Art</span>
+            </button>
+
+            <button
+              type="button"
               className={`dash-mobile-bottom-btn ${activeTab === 'design' ? 'dash-mobile-bottom-btn-active' : ''}`}
               onClick={() => setActiveTab('design')}
             >
